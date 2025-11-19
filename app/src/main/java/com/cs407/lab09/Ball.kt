6a4bb13721516998dff137c1a35aca73ff31147b
JensLundsgaard <jenslundsgaard7@gmail.com>
2025-11-18 21:38:18 -0600

    Milestone 1 done for demo grading

--- app/src/main/java/com/cs407/lab09/Ball.kt
+++ app/src/main/java/com/cs407/lab09/Ball.kt
@@ -1,5 +1,7 @@
 package com.cs407.lab09
 
+import android.util.Log
+
 /**
  * Represents a ball that can move. (No Android UI imports!)
  *
@@ -13,8 +15,8 @@ class Ball(
     private val backgroundHeight: Float,
     private val ballSize: Float
 ) {
-    var posX = 0f
-    var posY = 0f
+    var posX = backgroundWidth / 2
+    var posY = backgroundHeight / 2
     var velocityX = 0f
     var velocityY = 0f
     private var accX = 0f
@@ -22,8 +24,9 @@ class Ball(
 
     private var isFirstUpdate = true
 
+
     init {
-        // TODO: Call reset()
+        reset()
     }
 
     /**
@@ -37,6 +40,23 @@ class Ball(
             accY = yAcc
             return
         }
+        checkBoundaries()
+        Log.d("helloworld", "x: " + posX.toString() + ", y: " + posY.toString() )
+
+
+        val oldAccX = accX
+        val oldAccY = accY
+
+        accX += ((xAcc - accX)) * (dT) * 50
+        accY += ((yAcc - accY)) * (dT) * 50
+        val oldVelocityX = velocityX
+        val oldVelocityY = velocityY
+        velocityX += (oldAccX * dT)
+        velocityY += (oldAccY * dT)
+
+        posX += (oldVelocityX * dT)
+        posY += (oldVelocityY * dT)
+
 
     }
 
@@ -46,8 +66,24 @@ class Ball(
      * boundary should be set to 0.
      */
     fun checkBoundaries() {
-        // TODO: implement the checkBoundaries function
-        // (Check all 4 walls: left, right, top, bottom)
+        val rad = ballSize/2
+        if(posX - rad < 0f){
+            posX = 0.001f + rad
+            velocityX = 0f
+            accX = 0f
+        } else if (posY - rad < 0f){
+            posY = 0.001f + rad
+            velocityY = 0f
+            accY = 0f
+        } else if (posY + rad > backgroundHeight){
+            posY = backgroundHeight - (0.001f + rad)
+            velocityY = 0f
+            accY = 0f
+        } else if (posX + rad > backgroundWidth){
+            posX = backgroundWidth - (0.001f + rad)
+            velocityX = 0f
+            accX = 0f
+        }
     }
 
     /**
@@ -57,5 +93,13 @@ class Ball(
     fun reset() {
         // TODO: implement the reset function
         // (Reset posX, posY, velocityX, velocityY, accX, accY, isFirstUpdate)
+        posX = backgroundWidth / 2
+        posY = backgroundHeight / 2
+        velocityX = 0f
+        velocityY = 0f
+        accX = 0f
+        accY = 0f
+        isFirstUpdate = true
+
     }
 }
--- app/src/main/java/com/cs407/lab09/MainActivity.kt
+++ app/src/main/java/com/cs407/lab09/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -35,6 +36,8 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.cs407.lab09.R
 import com.cs407.lab09.ui.theme.Lab09Theme
@@ -62,14 +65,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen(viewModel: BallViewModel) {
+    val context = LocalContext.current
     // TODO: Initialize the sensorManager
     val sensorManager = remember {
-        // ... getSystemService ...
+        getSystemService(context, SensorManager::class.java) as SensorManager
     }
 
     // TODO: Get the gravitySensor
     val gravitySensor = remember {
-        // ... getDefaultSensor ...
+        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
     }
 
     // This effect runs when the composable enters the screen
@@ -79,7 +83,7 @@ fun GameScreen(viewModel: BallViewModel) {
             override fun onSensorChanged(event: SensorEvent?) {
                 // TODO: Pass the sensor event to the ViewModel
                 event?.let {
-                    // ...
+                    viewModel.onSensorDataChanged(it)
                 }
             }
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -90,7 +94,7 @@ fun GameScreen(viewModel: BallViewModel) {
         // TODO: Register the sensor listener
         // (Don't forget to add a null check for gravitySensor!)
         if (gravitySensor != null) {
-            // ... sensorManager.registerListener ...
+            sensorManager.registerListener(listener, gravitySensor, 1)
         }
 
         // onDispose is called when the composable leaves the screen
@@ -98,7 +102,7 @@ fun GameScreen(viewModel: BallViewModel) {
             // TODO: Unregister the sensor listener
             // (Don't forget to add a null check for gravitySensor!)
             if (gravitySensor != null) {
-                // ... sensorManager.unregisterListener ...
+                sensorManager.unregisterListener(listener)
             }
         }
     }
@@ -108,7 +112,7 @@ fun GameScreen(viewModel: BallViewModel) {
         // 1. The Reset Button
         Button(
             onClick = {
-                // TODO: Call the reset function on the ViewModel
+                viewModel.reset()
             },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
@@ -122,10 +126,9 @@ fun GameScreen(viewModel: BallViewModel) {
         val ballSizePx = with(LocalDensity.current) { ballSize.toPx() }
 
         // TODO: Collect the ball's position from the ViewModel
-        // val ballPosition by viewModel.ballPosition.collectAsStateWithLifecycle()
+         val ballPosition by viewModel.ballPosition.collectAsStateWithLifecycle()
 
-        // Placeholder, remove when TODO is done:
-        val ballPosition = Offset.Zero
+
 
 
         Box(
@@ -137,8 +140,8 @@ fun GameScreen(viewModel: BallViewModel) {
                     contentScale = ContentScale.FillBounds
                 )
                 .onSizeChanged { size ->
-                    // TODO: Tell the ViewModel the size of the field
-                    // viewModel.initBall(...)
+
+                    viewModel.initBall(size.width.toFloat(),size.height.toFloat(), ballSizePx)
                 }
         ) {
             // 3. The Ball
